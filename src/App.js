934dea1c26266731286a184400e80ba39f7ac4ba
Matheus Boari <matheusboari@gmail.com>
2019-11-08 14:50:30 -0300

    Hook useEffect :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTech] = useState(['ReactJS', 'React Native']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
-    setTech([...techs, newTech]);
+    setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
